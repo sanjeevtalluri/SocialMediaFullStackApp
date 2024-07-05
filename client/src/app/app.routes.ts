37bad17e7edc7bc71c507499856501abcddea8b9
sanjeevtalluri <sanjeevtalluri@gmail.com>
2024-07-05 16:03:59 +0530

    Added Candeactivate route and Update profile

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,34 +3,47 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { authGuard } from './guards/auth.guard';
+import { preventUnsavedChangesGuard } from './guards/prevent-unsaved-changes.guard';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', component: HomeComponent },
   {
-    path:'errors',component:TestErrorsComponent
+    path: 'errors',
+    component: TestErrorsComponent,
   },
   {
-    path:'not-found',component:NotFoundComponent
+    path: 'not-found',
+    component: NotFoundComponent,
   },
   {
-    path:'server-error',component:ServerErrorComponent
+    path: 'server-error',
+    component: ServerErrorComponent,
   },
   {
-    path:'',
-    runGuardsAndResolvers:'always',
-    canActivate:[authGuard],
-    children:[
-        { path: 'members', component: MemberListComponent, canActivate:[authGuard]},
-        { path: 'members/:username', component: MemberDetailComponent },
-        { path: 'lists', component: ListsComponent },
-        { path: 'messages', component: MessagesComponent },
-        { path: '**', component: HomeComponent,pathMatch:'full' }
-    ]
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'members',
+        component: MemberListComponent,
+        canActivate: [authGuard],
+      },
+      { path: 'members/:username', component: MemberDetailComponent },
+      {
+        path: 'member/edit',
+        component: MemberEditComponent,
+        canDeactivate: [preventUnsavedChangesGuard],
+      },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: '**', component: HomeComponent, pathMatch: 'full' },
+    ],
   },
-  
 ];
