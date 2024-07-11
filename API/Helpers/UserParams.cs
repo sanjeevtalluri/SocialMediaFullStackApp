6c3f2f7b6e76d97ec74f5f7c381b6dfae44d2d91
sanjeevtalluri <sanjeevtalluri@gmail.com>
2024-07-11 17:33:32 +0530

    Added Filter functionality

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -17,10 +17,10 @@ namespace API.Helpers
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
 
-        // public string? CurrentUsername { get; set; }
-        // public string? Gender { get; set; }
-        // public int MinAge { get; set; } = 18;
-        // public int MaxAge { get; set; } = 150;
-        // public string OrderBy { get; set; } = "lastActive";
+        public string? CurrentUsername { get; set; }
+        public string? Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 100;
+        public string OrderBy { get; set; } = "lastActive";
     }
 }
